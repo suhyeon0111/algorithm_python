73211fcc429eb6e124e5238950f26decd9564599
DESKTOP-ME54L0C\Lab's <81307924+suhyeon0111@noreply.github.com>
2021-09-27 23:37:51 +0900

    백준 10815번 숫자 탐색
    -실버4
    -python

---
+++ 이진탐색/10815.py
@@ -0,0 +1,41 @@
+import sys
+
+#상근이 카드 N개
+N = int(sys.stdin.readline())
+N_card = list(map(int, sys.stdin.readline().split()))
+#새로운 카드 M개
+M = int(sys.stdin.readline())   
+M_card = list(map(int, sys.stdin.readline().split()))
+
+#정렬
+N_card.sort()
+
+#새로 담아줄 list
+result = []
+
+#비교해주기
+for i in range(len(M_card)):
+    start = 0
+    end = len(N_card) - 1
+    result.append(0)    
+    while start <= end:
+        #중간 값을 중심으로 탐색하기
+        mid = (start + end) // 2
+    
+        #중간 값과 같은 값이 있다면 0을 빼주고 1을 넣어줌, 다음 M_card를 탐색하기 위해 루프를 탈출
+        if N_card[mid] == M_card[i]: 
+            result.pop()
+            result.append(1) 
+            break     
+        
+        #중간 값이 M_card보다 크다면 end를 감소 후, 다시 탐색
+        elif N_card[mid] > M_card[i]:
+            end = mid - 1 #end 위치 바꿔주기
+
+        #중간 값이 M_card보다 작다면 start를 증가 후, 다시 탐색
+        elif N_card[mid] < M_card[i]:
+            start = mid + 1 #start 위치 바꿔주기
+
+
+cardresult = " ".join(map(str, result))
+print(cardresult)
