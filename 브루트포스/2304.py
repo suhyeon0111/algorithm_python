e2a50dd1425dd9877f6c8f1ade1af7779af7d837
suhyeon <soo4833@naver.com>
2021-09-30 22:36:43 +0900

    백준 2304번 창고다각형 문제 (예시1은 출력은 하지만 다른 예시에 적용이 안됨 - 수정중)

--- 브루트포스/2304.py
+++ 브루트포스/2304.py
@@ -1,49 +1,52 @@
 import sys
 
-
 N = int(sys.stdin.readline()) 
 
 #기둥의 정보를 한번에 담을 list선언
 Pillar = [] 
 
+#입력받기
 for i in range(N):
     L, H = list(map(int, sys.stdin.readline().split()))
     Pillar.append([L, H])
 
 
-#1번째 인덱스에 대해서만 내림차순 정렬(높이에 대해 오름차순)
-Pillar.sort( key=lambda x: -x[1])
+#0번째 인덱스에 대해서만 오름차순 정렬
+Pillar.sort( key=lambda x: x[0])
 
-
-max_height = Pillar[0][1]#가장 높은 기둥의 높이
-min_height = Pillar[1][1]#그 다음 높은 기둥의 높이
-
-L_start = Pillar[0][0] #가장 높은 기둥의 가로 시작점
-#max_l = max(Pillar[0][0],Pillar[0][1]) #그 다음 기둥의 가로 시작점
-
-Area = max_height - min_height
-print(Area)
-
+#가장 높은 기둥 찾기
+high_H = Pillar[0][1] 
 for i in range(1, len(Pillar)):
-    h = Pillar[i][1]
-    l = Pillar[i][0]    
+    #다른 기둥이 더 클 경우
+    if high_H <= Pillar[i][1]:
+        high_H = Pillar[i][1] #더 큰 기둥으로 바꿔주기
+        high_H_index = i #가장 큰 기둥의 인덱스 저장
 
-    if  L_start > l and l < max_l:
-        continue
-    else:
-        max_l = l
-        L_area = abs(max_l - L_start) + 1 #직사각형의 가로 길이
+#왼쪽 탐색
+height = Pillar[0][1]
+min_L = Pillar[0][0]
+Area = Pillar[high_H_index][1]
 
-        if max_l == Pillar[-1][0]:
-            Area += ((min_height) * L_area) #직사각형의 넓이
-            break
+for i in range(high_H_index + 1):     
+    if height < Pillar[i][1]:
+        Area_L = Pillar[i][0] - min_L
+        Area += (height * Area_L)
+        height = Pillar[i][1]
+        min_L = Pillar[i][0]
 
-        else:
-            Area += ((max_height - min_height) * L_area) #직사각형의 넓이
+#오른쪽 탐색
+height_R = Pillar[-1][1]  
+max_L = Pillar[-1][0]
 
-            max_height = min_height 
-            min_height = h
+for i in range(len(Pillar)-1, high_H_index - 1, -1):
+    if height_R < Pillar[i][1]:
+        Area_L = max_L - Pillar[i][0]
+        Area += (height_R * Area_L)
+        height_R = Pillar[i][1]
+        max_L = Pillar[i][0]
 
-#넓이 출력
+
 print(Area)
+
+
 
