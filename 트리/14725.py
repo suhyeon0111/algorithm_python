418dc50825ba985468c850f13d50cfea01bd44bf
SuHyeon Lee <soo4833@naver.com>
2022-09-14 23:54:46 +0900

    백준 14725.py
    완전 탐색 풀이
    추후 트라이로 풀이 필요

--- 트리/14725.py
+++ 트리/14725.py
@@ -3,17 +3,28 @@
 import sys
 
 n = int(sys.stdin.readline())
-food = []
+food_infomation = []
 for _ in range(n):
-    infomation = list(sys.stdin.readline().split())
-    food.append(infomation[1:])
+    infomation = list(sys.stdin.readline().rsplit())
+    food_infomation.append(infomation[1:])  # 숫자 제거
 
-food.sort()
+food_infomation.sort()
 
-result = []  # 결과값 저장
-dash = "--"  # 층 나타내기
-
+floor = "--"
+result = []
 for i in range(n):
-    if i == 0:  # 처음은 중복이 없으므로 바로 append
-        for j in range(len(food[i])):
-            result.append(dash * j + (food[i][j]))
+    if i == 0:
+        for j in range(len(food_infomation[i])):
+            result.append(floor * j + food_infomation[i][j])
+    else:
+        idx = 0  # 같은 원소의 개수 저장
+        for j in range(len(food_infomation[i])):
+            if food_infomation[i - 1][j] != food_infomation[i][j] or len(food_infomation[i - 1]) <= j:
+                break
+            else:
+                idx = j + 1
+        for j in range(idx, len(food_infomation[i])):
+            result.append(floor * j + food_infomation[i][j])
+
+for x in result:
+    print(x)
